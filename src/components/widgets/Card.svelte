b6412569d5964722f9916430a5372b8a91596458
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2026-03-05 23:15:52 +0530

    updates

---
+++ src/components/widgets/Card.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { HoverTilt } from "hover-tilt";
+</script>
+
+<HoverTilt
+  shadow
+  scaleFactor={1.1}
+  class="[&::part(container)]:rounded-[4.55%/3.5%]"
+>
+  <slot />
+</HoverTilt>
